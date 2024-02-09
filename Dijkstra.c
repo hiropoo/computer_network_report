196e8f975e759a09e784899784d7974e69bb0c13
hiropoo <125180473+hiropoo@users.noreply.github.com>
2024-02-09 22:07:46 +0900

    コンネットレポート用 テスト

---
+++ Dijkstra.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int pathNum = -1;    // パスの数
+    int nodeNum = -1;    // ノードの数
+
+    // パスの数とノードの数を入力
+    scanf("%d %d", &pathNum, &nodeNum);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
